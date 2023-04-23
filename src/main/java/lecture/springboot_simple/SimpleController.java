b0352c51257f4192a330e4ad05f84b819c6cc49c
Polonski Iouri <iouri.polonski@e-velopment.de>
2023-04-23 19:28:40 +0200

    ausgabe angepasst

--- src/main/java/lecture/springboot_simple/SimpleController.java
+++ src/main/java/lecture/springboot_simple/SimpleController.java
@@ -18,7 +18,7 @@ public class SimpleController {
 
     @GetMapping("/greet/{name}")
     public ResponseEntity<String> greetUser(@PathVariable String name) {
-        String res = "Hello " + name + " from AWS";
+        String res = "Hello " + name;
         return ResponseEntity.ok(res);
     }
 
